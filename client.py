5c89ea940e136a493b615770b5dad33863cb36f7
atoy322 <atoy_0322@yahoo.co.jp>
2021-04-28 20:11:15 +0900

    update client.py

--- client.py
+++ client.py
@@ -1,7 +1,6 @@
 import socket
 import struct
 import io
-import time#
 
 from PIL import Image
 
@@ -13,7 +12,6 @@ s.connect(("atoy322.ddns.net", 8000))
 
 try:
     while True:
-        t1 = time.time()#
         received = 0
         img_buf = b""
         size_data = s.recv(4)
@@ -22,16 +20,12 @@ try:
         while True:
             data = s.recv(size-received)
             received += len(data)
-            #print("\r", "expect: {:6d}   real: {:6d}".format(size, received), end="")
             img_buf += data
             if size <= received:
                 break
 
         img = Image.open(io.BytesIO(img_buf))
-        print(1/(time.time()-t1))#
-        img.save("img.jpg")#
 
 
 except Exception as e:
-    raise e#
     s.close()
