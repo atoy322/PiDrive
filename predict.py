83fc39c5e7f9b9c67c57c5eac13eb705a5e738fd
atoy322 <atoy_0322@yahoo.co.jp>
2021-07-30 11:25:02 +0900

    update

--- predict.py
+++ predict.py
@@ -1,5 +1,37 @@
-import cv2
+import math
 
+import cv2
+from PIL import Image
+import numpy as np
+from chainer.serializers import load_npz
+
+from model import LineDetector
+
+
+
+model = LineDetector()
+load_npz("model.npz", model)
+
+
+def predict(img):
+    img_array = np.array(img)
+    img_ = img.crop((0, img.height//2, img.width, img.height)).convert("L")
+    img_ = img_.resize((64, 24))
+    X = np.array(img_, dtype=np.float32)
+    X = X.reshape(1, 1, *X.shape) / 255
+    y = model(X)[0].array * 5
+    m = (20*5 - 12*5) / (y[1] - 32*5) # xの変化量分のyの変化量
+    theta = math.atan(m) 
+    theta = math.degrees(theta)
+    if theta >= 0:
+        theta = 90 - theta
+    else:
+        theta = -(90 + theta)
+    color = (167, 254, 113)
+    img_array = cv2.circle(img_array, (y[0], 20*5 + 120), 5, color, -1)
+    img_array = cv2.circle(img_array, (y[1], 12*5 + 120), 5, color, -1)
+    img_array = cv2.circle(img_array, (y[2], 4*5 + 120), 5, color, -1)
+    return Image.fromarray(img_array), theta
 
 cam = cv2.VideoCapture(0)
 
@@ -9,9 +41,9 @@ while True:
         if not flag:
             continue
 
-        res = cv2.cvtColor(res, cv2.COLOR_RGB2GRAY)
-        cv2.resize(res, (640//5))
-        print(res.shape)
+        res = cv2.resize(res, (320, 240))
+        image = Image.fromarray(res)
+        predict(image[1])
     except Exception as e:
         print(e)
         break
--- predict_remote.py
+++ predict_remote.py
@@ -46,31 +46,27 @@ def image_preprocess(img):
 def image_preprocess(img):
     img_array = np.array(img)
     img_ = img.crop((0, img.height//2, img.width, img.height)).convert("L")
-    img_ = img_.resize((64, 24)) #################################################
-    array = np.array(img_, dtype=np.float32)
-    #y = model(array.reshape(-1, *array.shape).transpose(0, 3, 1, 2))#n 3 24 64
-    X = array#.flatten()
-    X = X.reshape(-1, 1, *X.shape) / 255
+    img_ = img_.resize((64, 24))
+    X = np.array(img_, dtype=np.float32)
+    X = X.reshape(1, 1, *X.shape) / 255
     y = model(X)[0].array * 5
-    #m = (40) / (y[1] - y[0])
-    m = (40) / (y[1] - 32*5)
+    m = (20*5 - 12*5) / (y[1] - 32*5) # xの変化量分のyの変化量
     theta = math.atan(m) 
     theta = math.degrees(theta)
     if theta >= 0:
         theta = 90 - theta
     else:
         theta = -(90 + theta)
-    print(theta)
-    img_array = cv2.circle(img_array, (y[0], 20*5 + 120), 5, (255, 0, 0), -1)
-    img_array = cv2.circle(img_array, (y[1], 12*5 + 120), 5, (255, 0, 0), -1)
-    img_array = cv2.circle(img_array, (y[2], 4*5 + 120), 5, (255, 0, 0), -1)
-    img_array = cv2.line(img_array, (y[0], 20*5 + 120), (y[1], 12*5 + 120), (255, 0, 0), 3)
+    color = (167, 254, 113)
+    img_array = cv2.circle(img_array, (y[0], 20*5 + 120), 5, color, -1)
+    img_array = cv2.circle(img_array, (y[1], 12*5 + 120), 5, color, -1)
+    img_array = cv2.circle(img_array, (y[2], 4*5 + 120), 5, color, -1)
     return Image.fromarray(img_array), theta
 
 
 class Preview(Window):
     def __init__(self, ip, width=720, height=480):
-        super().__init__(width=width*2, height=height)
+        super().__init__(width=width, height=height)
         self.control_sock = socket.socket()
         self.stream_sock = socket.socket()
         self.stream_sock.connect((ip, 8000))
@@ -96,14 +92,14 @@ class Preview(Window):
         self.preprocessed, pred = image_preprocess(self.img) # To save
         preprocessed = self.preprocessed.transpose(Image.FLIP_TOP_BOTTOM) # To show
 
-        img = ImageData(W, H, "RGB", img.tobytes("raw", "RGB"))
-        preprocessed = ImageData(W, H, "RGB", preprocessed.tobytes("raw", "RGB"))
+        img = ImageData(W, H, "RGB", preprocessed.tobytes("raw", "RGB"))
         img.blit(0, 0)
-        preprocessed.blit(W, 0)
 
         pred = min(30, max(pred*0.3, -30))
         self.control_sock.send(b"SPEED:40")
         self.control_sock.send(f"STEER:{int(pred)}".encode())
+
+        self.set_caption("FPS: {:3.5f} [frame/s]".format(1/dt))
 
     def on_key_press(self, symbol, modifiers):
         if symbol == key.UP:
