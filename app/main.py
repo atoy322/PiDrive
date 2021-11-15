ebd9c2f2d5e9754e45e500fc40160999f4e14e80
atoy322 <atoy_0322@yahoo.co.jp>
2021-11-15 14:52:50 +0900

    update

---
+++ app/main.py
@@ -0,0 +1,16 @@
+import socket
+
+from threading import Thread
+
+from function_http import HTTPServer
+from function_ws import thread
+
+
+t = Thread(target=thread, daemon=True)
+t.start()
+
+server_sock = socket.socket()
+server_sock.bind(("", 8000))
+server_sock.listen(1)
+server = HTTPServer(server_sock)
+server.client_loop()
